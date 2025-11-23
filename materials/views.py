a1ee16d885a0c70a988d966b7d469de64f455cd1
Виктор Скрипник <viktor_ds@mail.ru>
2025-11-23 09:41:31 +0800

    hw352 ver10

--- materials/views.py
+++ materials/views.py
@@ -81,7 +81,7 @@ class CourseUpdateApiView(UpdateAPIView):
 
     def perform_update(self, serializer):
         course = serializer.save()
-        result = send_update().delay(course.id)
+        result = send_update(course_id=course.id).delay(course.id)
         return result
 
 
